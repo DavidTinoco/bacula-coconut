659282ae4f4df7d75b56353895a2eaeaaea6bfb4
David Tinoco <daviid.tc@gmail.com>
2018-01-12 13:25:46 +0100

    ahora se guarda en la propia maquina

--- installed-packages.sh
+++ installed-packages.sh
@@ -4,7 +4,7 @@
 # Author: David Tinoco Castillo
 # Version 0.1
 
-ssh -i /root/.ssh/copias root@10.0.0.9 'dpkg --get-selections | grep -v deinstall | cut -f1' > /copias/installedPackages/mickey`date +%Y%m%d`
-ssh -i /root/.ssh/copias root@10.0.0.10 'dpkg --get-selections | grep -v deinstall | cut -f1' > /copias/installedPackages/minnie`date +%Y%m%d`
-ssh -i /root/.ssh/copias root@10.0.0.6 'rpm -qa' > /copias/installedPackages/donald`date +%Y%m%d`
+ssh -i /root/.ssh/copias root@10.0.0.9 'dpkg --get-selections | grep -v deinstall | cut -f1 > /root/installed-packages.txt'
+ssh -i /root/.ssh/copias root@10.0.0.10 'dpkg --get-selections | grep -v deinstall | cut -f1 > /root/installed-packages.txt'
+ssh -i /root/.ssh/copias root@10.0.0.6 'rpm -qa > /root/installed-packages.txt'
 
